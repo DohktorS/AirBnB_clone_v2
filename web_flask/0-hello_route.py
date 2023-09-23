cd2dee3e37145bdfb0e481a431e7ba1f66b3abdc
DohktorS <somtosherill66@gmail.com>
2023-09-23 00:02:24 +0000

    App server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application
-and would be listening on 0.0.0.0, port 5000"""
-from flask import Flask
+"""This module uses Flask and starts a web application"""
 
+
+from flask import Flask
 app = Flask(_name_)
 
 
-@app.route("/", strict_slashes=False)
-def function():
-    """main or home route
-
-    Rules that end with a slash are “branches”, others
-    are “leaves”. If << strict_slashes >> is enabled
-    (the default), visiting a branch URL without a
-    trailing slash will redirect to the URL with a slash appended.
-
-    Returns:
-            [string]: [display “Hello HBNB!”]
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello world for flask"""
     return "Hello HBNB!"
 
-
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port="5000")
